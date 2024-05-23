efb31ff8b317040414841b6f386f8099173d90f8
Smileycorp <callum.j.longbottom@gmail.com>
2024-05-23 19:07:51 +0100

    add attribute modifier handling for stats

--- src/main/java/net/smileycorp/lootweapons/common/item/LootSword.java
+++ src/main/java/net/smileycorp/lootweapons/common/item/LootSword.java
@@ -1,6 +1,13 @@
 package net.smileycorp.lootweapons.common.item;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.smileycorp.lootweapons.common.attributes.ElementalWeaponAttributes;
@@ -35,6 +42,17 @@ public class LootSword extends SwordItem implements LootItem {
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> lines, TooltipFlag flag) {
         lines.add(getDescription(stack, Component.translatable("text.lootweapons.item.sword")));
+    }
+    
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
+        ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+        float[] stats = getLootData(stack).getStats();
+        float attack = (stats.length > 0) ? stats[0] : 1;
+        float speed = (stats.length > 1) ? 1-stats[1] : 1;
+        builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", attack * 5f, AttributeModifier.Operation.ADDITION));
+        builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Weapon modifier", speed * -2.4f, AttributeModifier.Operation.ADDITION));
+        return builder.build();
     }
     
 }
